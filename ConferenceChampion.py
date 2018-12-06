a799b14caec5c8150d51ec76be580106330d3d6b
wilsonator <36900506+wilsonator@users.noreply.github.com>
2018-12-05 21:31:20 -0500

    Create ConferenceChampion.py

---
+++ ConferenceChampion.py
@@ -0,0 +1,29 @@
+#this script will help you attend security conferences like Schmoocon. 
+#Use it to start a loop that will check the time and if it is the defined time that the conference registration open (ex. December 13th 2018 at Noon EST) it'll open 5 webpages
+#
+
+#import demz libs
+import webbrowser
+import datetime
+import time
+
+myTime = datetime.datetime.now()
+registrationOpen = datetime.datetime(2018, 12, 5, 21, 13, 0)
+url = "http://landing.shmoocon.org/" 
+
+print(registrationOpen)
+
+while datetime.datetime.now().replace(microsecond=0) != registrationOpen:
+	time.sleep(0.01)
+	print(datetime.datetime.now())
+
+else:
+	webbrowser.open(url)
+	webbrowser.open(url)
+	webbrowser.open(url)
+	webbrowser.open(url)
+	webbrowser.open(url)
+	webbrowser.open(url)
+	print("good luck, Chuck! :)")
+
+
